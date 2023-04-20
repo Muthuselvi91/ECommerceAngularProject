229d57bf08bb269e1e4c2b95ed0c4052ac624104
MuthuselviRamasamy <MuthuselviRamasamy@DYS10107-108>
2023-04-20 18:48:57 +0530

    Dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AllProductsComponent } from './all-products/all-products.component';
+import { ElectronicsComponent } from './electronics/electronics.component';
+import { HeaderComponent } from './header/header.component';
+import { FashionComponent } from './fashion/fashion.component';
+import { HomeAppliancesComponent } from './home-appliances/home-appliances.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllProductsComponent,
+    ElectronicsComponent,
+    HeaderComponent,
+    FashionComponent,
+    HomeAppliancesComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
